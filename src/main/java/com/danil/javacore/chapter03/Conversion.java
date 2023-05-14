35c4f849502d07adf227769d447c0e9f7885eee4
Harbdrain <mail@d-demchenko.ru>
2023-05-14 19:52:25 +0300

    chapter03: Nineth example.

---
+++ src/main/java/com/danil/javacore/chapter03/Conversion.java
@@ -0,0 +1,25 @@
+package com.danil.javacore.chapter03;
+
+public class Conversion {
+    public static void main(String[] args) {
+        byte b;
+        int i = 257;
+        double d = 323.132;
+
+        System.out.println("Converting int into byte.");
+        b = (byte) i;
+        System.out.println("i: " + i);
+        System.out.println("b: " + b);
+
+        System.out.println("\nConverting double into int.");
+        i = (int) d;
+        System.out.println("d: " + d);
+        System.out.println("i: " + i);
+
+
+        System.out.println("\nConverting double into byte.");
+        b = (byte) d;
+        System.out.println("d: " + d);
+        System.out.println("b: " + b);
+    }
+}
