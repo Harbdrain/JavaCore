6f6197ebb9bc25e16057f53a87b6e502eea9253a
Harbdrain <mail@d-demchenko.ru>
2023-05-14 18:21:22 +0300

    chapter02: Third example

---
+++ src/main/java/com/danil/javacore/chapter02/IfSample.java
@@ -0,0 +1,25 @@
+package com.danil.javacore.chapter02;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+
+        if (x < y) {
+            System.out.println("x less than y.");
+        }
+
+        x *= 2;
+        if (x == y) {
+            System.out.println("x equals y.");
+        }
+
+        x *= 2;
+        if (x > y) {
+            System.out.println("x greater than y.");
+        }
+
+        if (x == y) {
+            System.out.println("Unreachable code.");
+        }
+    }
+}
