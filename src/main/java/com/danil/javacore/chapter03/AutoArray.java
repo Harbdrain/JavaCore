e7702ce49d522028ddf5ee043d9238f043520c7e
Harbdrain <mail@d-demchenko.ru>
2023-05-14 20:38:36 +0300

    chapter03: Twelfth example.

---
+++ src/main/java/com/danil/javacore/chapter03/AutoArray.java
@@ -0,0 +1,8 @@
+package com.danil.javacore.chapter03;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        System.out.println("Days in april: " + month_days[3]);
+    }
+}
