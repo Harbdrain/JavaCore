74a7722a98468df8b0b1933b857ed7eb0654fff1
Harbdrain <mail@d-demchenko.ru>
2023-05-14 18:17:09 +0300

    chapter02: Second example

---
+++ src/main/java/com/danil/javacore/chapter02/Example2.java
@@ -0,0 +1,12 @@
+package com.danil.javacore.chapter02;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num; // num is declared, but not defined.
+        num = 10; // num is defined.
+        System.out.println("Num: " + num);
+        num *= 2;
+        System.out.print("Num * 2 = ");
+        System.out.println(num);
+    }
+}
