f4561f57a86f2ce6d098ca1596f40b4422881687
Harbdrain <mail@d-demchenko.ru>
2023-05-14 18:16:57 +0300

    chapter02: First example

--- src/main/java/com/danil/javacore/App.java
+++
@@ -1,7 +0,0 @@
-package com.danil.javacore;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/com/danil/javacore/chapter02/Example.java
@@ -0,0 +1,7 @@
+package com.danil.javacore.chapter02;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
