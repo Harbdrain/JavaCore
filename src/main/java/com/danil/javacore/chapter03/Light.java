6b3cef9101c72997abdef97bc57e2f650b167380
Harbdrain <mail@d-demchenko.ru>
2023-05-14 19:07:39 +0300

    chapter03: First example.

---
+++ src/main/java/com/danil/javacore/chapter03/Light.java
@@ -0,0 +1,18 @@
+package com.danil.javacore.chapter03;
+
+public class Light {
+    public static void main(String[] args) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        lightspeed = 186000;
+        days = 1000;
+        seconds = days * 24 * 60 * 60;
+
+        distance = lightspeed * seconds;
+
+        System.out.println("Light will travel " + distance + " miles for " + days + " days.");
+    }
+}
