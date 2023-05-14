7e615706d371c04b98de2a9581b6f454832446c6
Harbdrain <mail@d-demchenko.ru>
2023-05-14 18:26:35 +0300

    chapter02: Fifth example

---
+++ src/main/java/com/danil/javacore/chapter02/BlockTest.java
@@ -0,0 +1,13 @@
+package com.danil.javacore.chapter02;
+
+public class BlockTest {
+    public static void main(String[] args) {
+        int x, y = 20;
+
+        for (x = 0; x < 10; x++) {
+            System.out.println("x: " + x);
+            System.out.println("y: " + y);
+            y -= 2;
+        }
+    }
+}
