d84bd55ebec457627490be077c5b3606405e2a50
Harbdrain <mail@d-demchenko.ru>
2023-05-15 19:56:10 +0300

    chapter07: Twentyth example.

---
+++ src/main/java/com/danil/javacore/chapter07/CommandLine.java
@@ -0,0 +1,9 @@
+package com.danil.javacore.chapter07;
+
+class CommandLine {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
