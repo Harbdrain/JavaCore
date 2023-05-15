0df1977788ad560bd2033cc28b2fffbf175a4971
Harbdrain <mail@d-demchenko.ru>
2023-05-15 16:17:59 +0300

    chapter05: Twentieth example.

---
+++ src/main/java/com/danil/javacore/chapter05/BreakLoop2.java
@@ -0,0 +1,15 @@
+package com.danil.javacore.chapter05;
+
+class BreakLoop2 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            if (i == 10) {
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Done");
+    }
+}
