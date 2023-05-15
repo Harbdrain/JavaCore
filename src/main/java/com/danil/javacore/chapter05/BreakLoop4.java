7f422d5086b5815542f0cd67ca20dd9ec93f7cba
Harbdrain <mail@d-demchenko.ru>
2023-05-15 16:28:37 +0300

    chapter05: Twenty second example.

---
+++ src/main/java/com/danil/javacore/chapter05/BreakLoop4.java
@@ -0,0 +1,17 @@
+package com.danil.javacore.chapter05;
+
+class BreakLoop4 {
+    public static void main(String[] args) {
+        outer: for (int i = 0; i < 3; i++) {
+            System.out.print(i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j == 10) {
+                    break outer;
+                }
+                System.out.print(j + " ");
+            }
+            System.out.println("Unreachable code");
+        }
+        System.out.println("\nDone");
+    }
+}
