8f79421c9559fc60fa05d4677c453aa4a57398cc
Harbdrain <mail@d-demchenko.ru>
2023-05-15 15:45:55 +0300

    chapter05: Sixteenth example.

---
+++ src/main/java/com/danil/javacore/chapter05/NoChange.java
@@ -0,0 +1,15 @@
+package com.danil.javacore.chapter05;
+
+class NoChange {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int num : nums) {
+            num *= 10;
+        }
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
