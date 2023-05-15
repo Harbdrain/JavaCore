efba57c5c71771e84c6f1b29a20ca9cecdfcc416
Harbdrain <mail@d-demchenko.ru>
2023-05-15 19:53:50 +0300

    chapter07: Nineteenth example.

---
+++ src/main/java/com/danil/javacore/chapter07/StringDemo3.java
@@ -0,0 +1,11 @@
+package com.danil.javacore.chapter07;
+
+class StringDemo3 {
+    public static void main(String[] args) {
+        String[] str = { "one", "two", "three" };
+
+        for (String s : str) {
+            System.out.println(s);
+        }
+    }
+}
