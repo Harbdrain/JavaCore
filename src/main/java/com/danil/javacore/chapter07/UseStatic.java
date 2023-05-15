1af66f9bef7dfb24c97910903ef005529c08debe
Harbdrain <mail@d-demchenko.ru>
2023-05-15 19:13:21 +0300

    chapter07: Twelveth example.

---
+++ src/main/java/com/danil/javacore/chapter07/UseStatic.java
@@ -0,0 +1,21 @@
+package com.danil.javacore.chapter07;
+
+class UseStatic {
+    static int a = 3;
+    static int b;
+
+    static void meth(int x) {
+        System.out.println("x: " + x);
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
+    static {
+        System.out.println("Static block initialization");
+        b = a * 4;
+    }
+
+    public static void main(String[] args) {
+        meth(42);
+    }
+}
