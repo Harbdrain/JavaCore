ad5ace68782c3ac30dd43d81174e0b847d86d295
Harbdrain <mail@d-demchenko.ru>
2023-05-15 20:01:12 +0300

    chapter07: Twenty first example.

---
+++ src/main/java/com/danil/javacore/chapter07/PassArray.java
@@ -0,0 +1,22 @@
+package com.danil.javacore.chapter07;
+
+class PassArray {
+    static void vaTest(int[] v) {
+        System.out.println("arg count: " + v.length);
+
+        for (int x : v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] n1 = { 10 };
+        int[] n2 = { 1, 2, 3 };
+        int[] n3 = {};
+
+        vaTest(n1);
+        vaTest(n2);
+        vaTest(n3);
+    }
+}
