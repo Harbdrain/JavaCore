bb6eec622bf6528c46f3c83fe81a036b55df7d96
Harbdrain <mail@d-demchenko.ru>
2023-05-15 15:04:44 +0300

    chapter05: Eithth example.

---
+++ src/main/java/com/danil/javacore/chapter05/DoWhile.java
@@ -0,0 +1,11 @@
+package com.danil.javacore.chapter05;
+
+class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+        do {
+            System.out.println("n: " + n);
+            n--;
+        } while (n > 0);
+    }
+}
