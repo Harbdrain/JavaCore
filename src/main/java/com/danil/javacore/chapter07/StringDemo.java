fcfc86f2ed1b4af1984e218e9398b56c06ac6a4b
Harbdrain <mail@d-demchenko.ru>
2023-05-15 19:49:24 +0300

    chapter07: Seventeenth example.

---
+++ src/main/java/com/danil/javacore/chapter07/StringDemo.java
@@ -0,0 +1,13 @@
+package com.danil.javacore.chapter07;
+
+class StringDemo {
+    public static void main(String[] args) {
+        String s1 = "First string";
+        String s2 = "Second string";
+        String s3 = s1 + "; " + s2;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
