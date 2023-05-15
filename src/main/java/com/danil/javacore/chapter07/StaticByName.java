36ac1ad17c608b3ac497d5b552de80ddf7dc45bd
Harbdrain <mail@d-demchenko.ru>
2023-05-15 19:19:25 +0300

    chapter07: Thirteenth example.

---
+++ src/main/java/com/danil/javacore/chapter07/StaticByName.java
@@ -0,0 +1,8 @@
+package com.danil.javacore.chapter07;
+
+class StaticByName {
+    public static void main(String[] args) {
+        StaticDemo.callme();
+        System.out.println("b: " + StaticDemo.b);
+    }
+}
