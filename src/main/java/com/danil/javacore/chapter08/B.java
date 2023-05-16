bbdd055c7eff520db8b69c68347f0db129b569a7
Harbdrain <mail@d-demchenko.ru>
2023-05-16 14:19:15 +0300

    chapter08: First example.

---
+++ src/main/java/com/danil/javacore/chapter08/B.java
@@ -0,0 +1,13 @@
+package com.danil.javacore.chapter08;
+
+class B extends A {
+    int k;
+
+    void showk() {
+        System.out.println("k: " + k);
+    }
+
+    void sum() {
+        System.out.println("i+j+k: " + (i + j + k));
+    }
+}
