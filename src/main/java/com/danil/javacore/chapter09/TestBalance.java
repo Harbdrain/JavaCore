3ee8f0f65298f712cff87feb5f57e5bfeed44925
Harbdrain <mail@d-demchenko.ru>
2023-05-16 17:11:23 +0300

    chapter09: Second example.

---
+++ src/main/java/com/danil/javacore/chapter09/TestBalance.java
@@ -0,0 +1,10 @@
+package com.danil.javacore.chapter09;
+
+import com.danil.javacore.chapter09.MyPack.Balance;
+
+class TestBalance {
+    public static void main(String[] args) {
+        Balance test = new Balance("J. J. Jasper", 99.88);
+        test.show();
+    }
+}
