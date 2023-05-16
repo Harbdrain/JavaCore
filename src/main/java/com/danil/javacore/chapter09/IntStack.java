71f708cd8dcb2208200b4006c6fc9630210c05c3
Harbdrain <mail@d-demchenko.ru>
2023-05-16 19:01:32 +0300

    chapter09: Tenth example.

--- src/main/java/com/danil/javacore/chapter09/IntStack.java
+++ src/main/java/com/danil/javacore/chapter09/IntStack.java
@@ -3,4 +3,8 @@ package com.danil.javacore.chapter09;
 interface IntStack {
     void push(int item);
     int pop();
+
+    default void clear() {
+        System.out.println("clear() is not implemented");
+    }
 }
