21a35d670ff5670f33c3f45b6e1c510ddcea8f82
Harbdrain <mail@d-demchenko.ru>
2023-05-24 13:02:46 +0300

    chapter18: Sixth example.

---
+++ src/main/java/com/danil/javacore/chapter18/ArrayDequeDemo.java
@@ -0,0 +1,21 @@
+package com.danil.javacore.chapter18;
+
+import java.util.ArrayDeque;
+
+class ArrayDequeDemo {
+    public static void main(String[] args) {
+        ArrayDeque<String> adq = new ArrayDeque<String>();
+        adq.push("A");
+        adq.push("B");
+        adq.push("D");
+        adq.push("E");
+        adq.push("F");
+
+        System.out.print("Stack:");
+
+        while (adq.peek() != null) {
+            System.out.print(adq.pop() + " ");
+        }
+        System.out.println();
+    }
+}
