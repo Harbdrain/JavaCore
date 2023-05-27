e62f73b306794ff029dc652755af1c5cb74d81f0
Harbdrain <mail@d-demchenko.ru>
2023-05-27 14:26:32 +0300

    chapter13: Seventh example.

---
+++ src/main/java/com/danil/javacore/chapter13/CopyFile.java
@@ -0,0 +1,48 @@
+package com.danil.javacore.chapter13;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class CopyFile {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        if (args.length != 2) {
+            System.out.println("Usage: CopyFile <inputfile> <outputfile>");
+            return;
+        }
+
+        try {
+            fin = new FileInputStream(args[0]);
+            fout = new FileOutputStream(args[1]);
+
+            do {
+                i = fin.read();
+                if (i != -1) {
+                    fout.write(i);
+                }
+            } while (i != -1);
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                if (fin != null) {
+                    fin.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+
+            try {
+                if (fout != null) {
+                    fout.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
