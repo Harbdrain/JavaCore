a18c21094229fedcaac05135e9c01bda5dc884c1
Harbdrain <mail@d-demchenko.ru>
2023-05-30 14:43:08 +0300

    chapter29: Fourth example.

---
+++ src/main/java/com/danil/javacore/chapter29/StreamDemo4.java
@@ -0,0 +1,21 @@
+package com.danil.javacore.chapter29;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+class StreamDemo4 {
+    public static void main(String[] args) {
+        ArrayList<Double> myList = new ArrayList<>();
+        myList.add(7.0);
+        myList.add(18.0);
+        myList.add(10.0);
+        myList.add(24.0);
+        myList.add(17.0);
+        myList.add(5.0);
+
+        Stream<Double> sqrtRootStream = myList.stream().map(e -> Math.sqrt(e));
+        double productOfSqrRoots = sqrtRootStream.reduce(1.0, (r, e) -> r * e);
+
+        System.out.println(productOfSqrRoots);
+    }
+}
