7ca9ee1dd1f54aeb786d07cfa01cb4dff43cf366
Harbdrain <mail@d-demchenko.ru>
2023-06-06 12:28:01 +0300

    chapter11: Seventh example.

---
+++ src/main/java/com/danil/javacore/chapter11/PC.java
@@ -0,0 +1,75 @@
+package com.danil.javacore.chapter11;
+
+class Q {
+    int n;
+    boolean valueSet = false;
+
+    synchronized int get() {
+        while (!valueSet) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Get: " + n);
+        valueSet = false;
+        notify();
+        return n;
+    }
+
+    synchronized void put(int n) {
+        while (valueSet) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.n = n;
+        valueSet = true;
+        System.out.println("Put: " + n);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    Q q;
+
+    Producer(Q q) {
+        this.q = q;
+        new Thread(this, "Producer").start();
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (true) {
+            q.put(i++);
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    Q q;
+
+    Consumer(Q q) {
+        this.q = q;
+        new Thread(this, "Consumer").start();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            q.get();
+        }
+    }
+}
+
+class PC {
+    public static void main(String[] args) {
+        Q q = new Q();
+        new Producer(q);
+        new Consumer(q);
+    }
+}
